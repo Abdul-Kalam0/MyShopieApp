fef89c44a9aa30783874f031f3c349d7bbac3233
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2025-11-01 10:19:01 +0530

    Fix(update): Updated the UI

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://my-shopie-app-5ls3.vercel.app"
+    "https://my-shopie-app-001.vercel.app"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
